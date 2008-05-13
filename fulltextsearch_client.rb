bc28ee85f042bf90d790fb900481f56c2c6d8283
Shinji Tanaka <shinji.tanaka@gmail.com>
2008-05-14 01:30:30 +0900

    fix

--- fulltextsearch_client.rb
+++ fulltextsearch_client.rb
@@ -6,8 +6,16 @@ require 'pp'
 
 $KCODE = 'UTF8'
 
+if ARGV.length < 1 then
+  puts "#{$0} word [bcount_factor] [scount_factor] [time_factor]"
+  exit
+end
+word = ARGV[0]
+bcount_factor = ARGV.length > 1 ? ARGV[1] : 1
+scount_factor = ARGV.length > 2 ? ARGV[2] : 1
+time_factor = ARGV.length > 3 ? ARGV[3] : 1
+
 there = DRbObject.new_with_uri('druby://localhost:12345') 
-pp there.search('これは', 0.2, 1, 1) 
-#pp there.search('Full', 0.2, 1, 1) 
+pp there.search(word, bcount_factor, scount_factor, time_factor)
 
 
